e78ecd393cd6bd773264d4d2e62d896d525b81c4
liangzw7 <1901543053@qq.com>
2020-02-21 20:21:37 +0800

    去除global mixins

--- src/widgets/demo/entry/welcome.js
+++ src/widgets/demo/entry/welcome.js
@@ -1,10 +1,4 @@
 import App from 'src/widgets/demo/views/welcome'
 import dolphinweex from 'src/js/dolphinweex.js'
-import globalMixin from 'src/mixins/global.js'
 
 Vue.use(dolphinweex)
-
-//全局混入
-Vue.mixin({
-  mixins: [globalMixin]
-})
